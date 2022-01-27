1fce87014fcf7b6eaeee50f593416efda8db78a4
Alan Balen Schio <alanbschio@gmail.com>
2022-01-27 13:08:40 -0400

    chore: backup

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,7 +3,7 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import VueDynamicForms from '@asigloo/vue-dynamic-forms';
-import '@asigloo/vue-dynamic-forms/src/styles/themes/material.scss';
+// import '@asigloo/vue-dynamic-forms/src/styles/themes/material.scss';
 
 Vue.use(VueDynamicForms);
 Vue.config.productionTip = false;
