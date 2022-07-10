49ea0d172e271296079193b343aa25b9c81b00a3
Alan Schio <alanbschio@gmail.com>
2022-07-10 00:58:22 -0300

    fix(core): fixin typo at baseurls creating

--- packages/core/src/services/api/simple.ts
+++ packages/core/src/services/api/simple.ts
@@ -13,15 +13,10 @@ const isAxiosRequestConfig = (config: AxiosRequestConfig | string): config is Ax
 }
 
 export const createSimpleCRUD = (config: AxiosRequestConfig | string): SimpleCRUD => {
-  console.log('createSimpleCRUD config', config)
   const _config = isAxiosRequestConfig(config) ? config : {
-    baseUrl: config
+    baseURL: config
   }
-  console.log('createSimpleCRUD _config', _config)
-
   const api = serviceFactory(_config as AxiosRequestConfig);
-  console.log('createSimpleCRUD api', api)
-
   return {
     read: read(api),
     create: create(api),
